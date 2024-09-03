6b7125d76d0cc236589582e47bc4e17ffcfb0b71
Patrick Mäder <padi.maeder@gmail.com>
2024-09-04 00:38:19 +0200

    Highlight active nav item

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,27 @@ export default {
 		container: {
 			center: true,
 		},
-		extend: {},
+		fontFamily: {
+			sans: ["noto-sans", "sans-serif"],
+			serif: ["cy", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				'my-sin': {
+					'50': '#fffdea',
+					'100': '#fff8c5',
+					'200': '#fff185',
+					'300': '#ffe446',
+					'400': '#ffd31b',
+					'500': '#fbaf00',
+					'600': '#e28800',
+					'700': '#bb5f02',
+					'800': '#984908',
+					'900': '#7c3c0b',
+					'950': '#481e00',
+				},
+			},
+		},
 	},
 	plugins: [],
 	darkMode: "selector",
